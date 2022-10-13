622ab621a1e7dceb0e70e5cc4d7029294e70ecb8
coleaiseosa <coleaiseosa@gmail.com>
2022-10-13 14:28:28 -0700

    added proper code indentation

--- passport.js
+++ passport.js
@@ -9,37 +9,37 @@ JWTStrategy = passportJWT.Strategy,
 ExtractJWT = passportJWT.ExtractJwt;
 
 passport.use(new LocalStrategy({
-    //LocalStrategy takes a username and password from the request body and uses mongoose to check the database for a user with the same username but the password doesn't get checked here.
-    usernameField: 'Username',
-    passwordField: 'Password'
+  //LocalStrategy takes a username and password from the request body and uses mongoose to check the database for a user with the same username but the password doesn't get checked here.
+  usernameField: 'Username',
+  passwordField: 'Password'
 }, (username, password, callback) => {
-    console.log(username + ' ' + password);
-    Users.findOne({ Username: username }, (error, user) => {
-        if(error) {
-            console.log(error);
-            return callback(error);
-        }
-      // if the username can't be found
-        if(!user) {
-            console.log('incorrect user name');
-            return callback(null, false, {message: 'Incorrect username or password.'});
-        }
+  console.log(username + ' ' + password);
+  Users.findOne({ Username: username }, (error, user) => {
+    if(error) {
+      console.log(error);
+      return callback(error);
+    }
+    // if the username can't be found
+    if(!user) {
+      console.log('incorrect user name');
+      return callback(null, false, {message: 'Incorrect username or password.'});
+    }
 
-        console.log('finished');
-        return callback(null, user);
-    });
+    console.log('finished');
+    return callback(null, user);
+  });
 }));
 
 //jwt is extracted from the header of the http request. jwt is called bearer token
 passport.use(new JWTStrategy ({
-    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-    secretOrKey: 'your_jwt_secret'
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey: 'your_jwt_secret'
 }, (jwtPayload, callback) => {
-    return Users.findById(jwtPayload._id)
-    .then((user) => {
-        return callback(null, user);
-    })
-    .catch((error) => {
-        return callback(error)
-    });
+  return Users.findById(jwtPayload._id)
+  .then((user) => {
+    return callback(null, user);
+  })
+  .catch((error) => {
+    return callback(error)
+  });
 }));
