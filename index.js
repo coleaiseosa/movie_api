8f054c5fff7767fa9e39935dfdb68659c9b06ff0
coleaiseosa <coleaiseosa@gmail.com>
2022-11-04 04:37:47 -0700

    added passport authentication to get movies

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get('/', (req, res) => {
 });
 
 // get all movies and return json object
-app.get('/movies', (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
   Movies.find()
   .then((movies) => {
     res.status(200).json(movies);
